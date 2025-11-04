0c38109545401ddcc7083ce05e87546128d8f304
Luis Mendez <slpmendez60@gmail.com>
2025-11-03 22:19:41 -0500

    feat: función con parámetros y con retorno implementada

---
+++ src/Multiply.java
@@ -0,0 +1,11 @@
+public class Multiply {
+    public static void main(String[] args) {
+        // TODO: Llamar a una función que multiplica dos números y devuelve el resultado
+        int resultado = multiplicar(4, 6);
+System.out.println("Resultado: " + resultado);
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+}
