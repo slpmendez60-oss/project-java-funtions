996dc0dba6f75967cb83096dd78977f2f990e557
Luis Mendez <slpmendez60@gmail.com>
2025-11-03 22:13:51 -0500

    feat: función con parámetros y sin retorno implementada

---
+++ src/PrintSum.java
@@ -0,0 +1,12 @@
+public class PrintSum {
+    public static void main(String[] args) {
+        // TODO: Llamar a una función que reciba dos números e imprima su suma
+        mostrarSuma(8, 12);
+    }
+
+    public static void mostrarSuma(int a, int b) {
+        int suma = a + b;
+        System.out.println("La suma es: " + suma);
+    }
+}
+
