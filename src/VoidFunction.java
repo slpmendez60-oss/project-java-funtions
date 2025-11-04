3b1b972ed485ef00542ef9330dff984721f4b321
Luis Mendez <slpmendez60@gmail.com>
2025-11-03 22:07:34 -0500

    feat: función sin parámetros y sin retorno implementada

---
+++ src/VoidFunction.java
@@ -0,0 +1,10 @@
+public class VoidFunction {
+    public static void main(String[] args) {
+        // TODO: Llamar a una función que no recibe ni devuelve nada
+        saludar(); // Llamamos la función
+    }
+
+    public static void saludar() {
+        System.out.println("¡Hola, mundo!");
+    }
+}
